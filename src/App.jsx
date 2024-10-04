ede7116d6922dee18bc072ab4007c7b96a8a24b2
Vinícius Campos <vscampos2k@gmail.com>
2024-10-04 19:00:14 -0300

    main
    Project DevMail.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Container from './components/Header/Container'
+import Sidebar from './components/Sidebar'
+import Bottombar from './components/bottombar'
+import EmailList from './components/EmailList'
+import EmailContent from './components/EmailContent'
+
+function App() {
+  const [openEmail, setOpenEmail] = useState(null)
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex flex-col md:flex-row gap-2 xl:gap-4 h-full relative ">
+          <Sidebar />
+          <EmailList setOpenEmail={setOpenEmail} />
+          {openEmail && <EmailContent email={openEmail} setOpenEmail={(setOpenEmail)} />}
+          <Bottombar />
+        </div>
+      </Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/EmailContent/index.jsx
@@ -0,0 +1,26 @@
+import EmailCloseButton from "./EmailCloseButton"
+import EmailData from "./EmailData"
+import EmailTitle from "./EmailTitle"
+
+export default function EmailContent({ email, setOpenEmail }) {
+    const sendDate = new Date(email.createdAt)
+    const openEmailDate = `${sendDate.toLocaleDateString()}, ${sendDate.toLocaleTimeString()}`
+    return(
+        <div
+        className="
+        p-2 md:p-4 absolute lg:relative h-full w-full
+        bg-gray-50 dark:bg-gray-700
+        text-gray-900 dark:dark:text-gray-50
+        grow-[2] basis-96
+        "
+        >
+            <EmailCloseButton onClick={() => setOpenEmail(null)}/>
+            <EmailTitle>{email.subject}</EmailTitle>
+            <EmailData>De: {`<${email.from}>`}</EmailData>
+            <EmailData>Para: {`<john.doe@emal.com>`}</EmailData>
+            <EmailData>Em: {openEmailDate}</EmailData>
+            <hr className="my-4"/>
+            {email.body}
+        </div>
+    )
+}
